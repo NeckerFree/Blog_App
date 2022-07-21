7f12020b733d3cf446f61b32bb2e99a37f200b1c
Elio Cortes <elio.cortes3000@gmail.com>
2022-07-20 20:18:56 -0500

    Implemented basic login form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @user=User.includes(:posts, :comments).find(params[:id])
+    @user = User.includes(:posts, :comments).find(params[:id])
   end
 
   def show
-   @post=User.includes(:posts, :comments).find(params[:id]).posts.find(params[:post_id])
+    @user = User.includes(:posts, :comments).find(params[:id])
+    @post = User.includes(:posts, :comments).find(params[:id]).posts.find(params[:post_id])
   end
 end
-
